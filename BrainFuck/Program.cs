7caa0300f34fae24d25df4127c50c1929b2e38b3
EgorBolshakov <Egor_Bolshakov_1991@mail.ru>
2022-04-17 18:56:35 +0300

    Добавьте файлы проекта.

---
+++ BrainFuck/Program.cs
@@ -0,0 +1,93 @@
+﻿public static class Program
+{
+    private static char[] _memory;
+    private static int _current = 0;
+
+    public static void Main()
+    {
+        _memory = new char[30000];
+
+        string program =
+            "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.+++++++++++++++++++++++++++++.+++++++..+++.-------------------------------------------------------------------------------.+++++++++++++++++++++++++++++++++++++++++++++++++++++++.++++++++++++++++++++++++.+++.------.--------.-------------------------------------------------------------------.-----------------------.";
+
+        foreach (var item in program)
+        {
+            if (item == '+')
+            {
+                NextCharValue();
+            }
+            if (item == '-')
+            {
+                PreviousCharValue();
+            }
+            if (item == '.')
+            {
+                DisplayCellValue();
+            }
+            if (item == '>')
+            {
+                NextCell();
+            }
+            if (item == '<')
+            {
+                PreviusCell();
+            }
+            if (item == ',')
+            {
+                InputValueInCell();
+            }
+            if (item == '[')
+            {
+                IfZeroNext();
+            }
+            if (item == ']')
+            {
+                IfNoZeroBack();
+            }
+        }
+
+    }
+
+    public static void NextCharValue()
+    {
+        _memory[_current]++;
+    }
+
+    public static void PreviousCharValue()
+    {
+        _memory[_current]--;
+    }
+    public static void DisplayCellValue()
+    {
+        Console.Write(_memory[_current]);
+    }
+    public static void NextCell()
+    {
+        _current++;
+    }
+    public static void PreviusCell()
+    {
+        _current--;
+    }
+
+    public static void InputValueInCell()
+    {
+        _memory[_current] = Convert.ToChar(Console.ReadLine());
+    }
+
+    public static void IfZeroNext()
+    {
+        while (_memory[_current] == 0)
+        {
+            _current++;
+        }
+    }
+
+    public static void IfNoZeroBack()
+    {
+        while (_memory[_current] != 0)
+        {
+            _current--;
+        }
+    }
+}
