a2896c6fe7e618d77c5893f64b08bc38b996db73
EgorBolshakov <Egor_Bolshakov_1991@mail.ru>
2022-05-03 20:16:13 +0300

    So close 3

--- BrainFuck/Program.cs
+++ BrainFuck/Program.cs
@@ -1,29 +1,34 @@
 ﻿public static class Program
 {
-
-
     public static void Main()
     {
         Repository BrainFuckCode = new Repository();
         DataOperations dataOperations = new DataOperations();
-        dataOperations.enumСodeBrainFuck(BrainFuckCode.program);
+        dataOperations.enumСodeBrainFuck(BrainFuckCode.Program);
 
     }
 }
 
 public class Repository
 {
-    public char[] _memory = new char[30000];
-    public int _current = 0;
-    //
-    public string program = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
-   
-    //"++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.+++++++++++++++++++++++++++++.+++++++..+++.-------------------------------------------------------------------------------.+++++++++++++++++++++++++++++++++++++++++++++++++++++++.++++++++++++++++++++++++.+++.------.--------.-------------------------------------------------------------------.----------------------*/ -.";
+    private char[] _memory;
+    private int _current;
+    private string _program;
+    public char[] Memory { get; set; }
+    public int Current { get; set; }
+    public string Program { get; set; }
+    public Repository()
+    {
+      Memory = new char[30000];
+      Current = 0;
+      Program = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+    }
+
 }
-
-public class DataOperations : Repository
+public class DataOperations
 {
-
+    Repository dataFromRepository = new Repository();
+    InputOutput inputOutput = new InputOutput();
     public void enumСodeBrainFuck(string BrainFuckCode)
     {
         for (int i = 0; i < BrainFuckCode.Length; i++)
@@ -59,34 +64,46 @@ public class DataOperations : Repository
     }
     public void NextCharValue()
     {
-        _memory[_current]++;
+        dataFromRepository.Memory[dataFromRepository.Current]++;
+      // так не рабит нихуя
+      //dataFromRepository.Memory[dataFromRepository.Current] = Convert.ToChar(Convert.ToInt32(dataFromRepository.Memory[dataFromRepository.Current]) + 1);
     }
 
     public void PreviousCharValue()
     {
-        _memory[_current]--;
+
+        dataFromRepository.Memory[dataFromRepository.Current]--;
+        // так не рабит нихуя
+        // dataFromRepository.Memory[dataFromRepository.Current] = Convert.ToChar(Convert.ToInt32(dataFromRepository.Memory[dataFromRepository.Current]) - 1);
+
     }
     public void DisplayCellValue()
     {
-        Console.Write(_memory[_current]);
+        inputOutput.OutputConsole(Convert.ToString(dataFromRepository.Memory[dataFromRepository.Current]));
     }
     public void NextCell()
     {
-        _current++;
+        if (dataFromRepository.Current<dataFromRepository.Memory.Length)
+        {
+            dataFromRepository.Current = dataFromRepository.Current + 1;
+        }
+        
     }
     public void PreviusCell()
     {
-        _current--;
+        if (dataFromRepository.Current>0)
+        {
+            dataFromRepository.Current = dataFromRepository.Current - 1;
+        }
+        
     }
     public void InputValueInCell()
-    // сделать ввод массива чаров - Так, стоп, а нахрена? Если у нас прием значения для одной ячейки, а не массива?
-
     {
-        _memory[_current] = Convert.ToChar(Console.ReadLine()); 
+        dataFromRepository.Memory[dataFromRepository.Current] = inputOutput.GetCharUser();
     }
     public int IfZeroNext(int PositionNumber, string BrainFuckCode)
     {
-        if (_memory[_current] == 0)
+        if (dataFromRepository.Memory[dataFromRepository.Current] == 0)
         {
             int NumberOfopenBrackets = 1;
             while (NumberOfopenBrackets != 0)
@@ -106,9 +123,9 @@ public class DataOperations : Repository
     }
     public int IfNoZeroBack(int PositionNumber, string BrainFuckCode)
     {
-        if (_memory[_current] != 0)
+        if (dataFromRepository.Memory[dataFromRepository.Current] != 0)
         {
-            int NumberOfopenBrackets = 1; 
+            int NumberOfopenBrackets = 1;
             while (NumberOfopenBrackets != 0)
             {
                 PositionNumber--;
@@ -123,6 +140,37 @@ public class DataOperations : Repository
 
             }
         }
-        return PositionNumber-1;
+        return PositionNumber - 1;
     }
+
+    
+}
+
+public class InputOutput
+{
+    public char GetCharUser()
+    {
+        while (true)
+        {
+            string userInput = GetStringUser();
+            if (char.TryParse(userInput, out char result))
+            {
+                return result;
+            }
+            // А так можно?
+            //if (char.TryParse(GetStringUser(), out char result))
+            //{
+            //    return result;
+            //}
+        }
+    }
+    public string GetStringUser()
+    {
+        return Console.ReadLine();
+    }
+    public void OutputConsole(string messageOrChar)
+    {
+        Console.WriteLine(messageOrChar);
+    }
+
 }
